12c628d72ead08cc7dcb426f10c25ab10935e13a
PURVA0202 <84826380+PURVA0202@users.noreply.github.com>
2022-09-13 22:18:10 +0530

    Time: 8 ms (60.27%), Space: 7.3 MB (77.25%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -4,44 +4,47 @@ public:
         
       
    
-        string res;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]>='a' && s[i]<='z')
-            {
+//         string res;
+//         for(int i=0;i<s.size();i++)
+//         {
+//             if(s[i]>='a' && s[i]<='z')
+//             {
                 
-                res+=s[i];
-            }
-            else if(s[i]>='A' && s[i]<='Z')
-            {
-                s[i]=tolower(s[i]);
-              
-                res+=s[i];
-            }
-            else if(s[i]>='0' && s[i]<='9')
-            {
-                res+=s[i];
-            }
-        }
-         
-        int i=0,j=res.size()-1;
-        
-       // cout<<res<<endl;
-//         if(res.size()==1)
-//         {
-//           return true;
+//                 res+=s[i];
 //             }
-             
-           
-//         else
+//             else if(s[i]>='A' && s[i]<='Z')
+//             {
+//                 s[i]=tolower(s[i]);
+              
+//                 res+=s[i];
+//             }
+//             else if(s[i]>='0' && s[i]<='9')
+//             {
+//                 res+=s[i];
+//             }
+//         }
+         
+//         int i=0,j=res.size()-1;
+        
+  
+//            while(i<=j)
 //         {
-           while(i<=j)
+//             if(res[i]!=res[j])   return false;
+//             i++;
+//             j--;
+//         }  
+//             return true;
+        
+        int i=0,j=s.size()-1;
+        while(i<j)
         {
-            if(res[i]!=res[j])   return false;
+            while(isalnum(s[i])==false && i<j)   i++;
+            while(isalnum(s[j])==false && i<j)  j--;
+            if(toupper(s[i])!=toupper(s[j]))    return false;
             i++;
             j--;
-        }  
-            return true;
+        }
+        return true;
             
        
        
