5cd533bbce02d5b815e97897bc1f22776d1b33a0
PURVA0202 <84826380+PURVA0202@users.noreply.github.com>
2022-09-20 17:35:52 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (90.72%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -3,46 +3,61 @@ public:
     vector<vector<int>> generate(int n)
     {
         
-    vector<vector<int>> ans;
+//     vector<vector<int>> ans;
    
-    int t=1;
-    while(t<=n)
-    {
-          if(t==1 || t==2)
-    {
-         vector<int> v;
-        for(int i=0;i<t;i++)
-        {
-            v.push_back(1);
-        }
-        ans.push_back(v);
-    }
-    else if(t==3)
-    {
-        vector<int> v;
-        v.push_back(1);
-        v.push_back(2);
-        v.push_back(1);
-        ans.push_back(v);
-    }
-    else{
-         vector<int> v;
-        v.push_back(1);
-        // for(int i=1;i<t-1;i++)
-        // {
-            for(int j=0;j<ans[t-2].size()-1;j++)
-            {
-                v.push_back((ans[t-2][j])+(ans[t-2][j+1]));
-            }
+//     int t=1;
+//     while(t<=n)
+//     {
+//           if(t==1 || t==2)
+//     {
+//          vector<int> v;
+//         for(int i=0;i<t;i++)
+//         {
+//             v.push_back(1);
+//         }
+//         ans.push_back(v);
+//     }
+//     else if(t==3)
+//     {
+//         vector<int> v;
+//         v.push_back(1);
+//         v.push_back(2);
+//         v.push_back(1);
+//         ans.push_back(v);
+//     }
+//     else{
+//          vector<int> v;
+//         v.push_back(1);
+//         // for(int i=1;i<t-1;i++)
+//         // {
+//             for(int j=0;j<ans[t-2].size()-1;j++)
+//             {
+//                 v.push_back((ans[t-2][j])+(ans[t-2][j+1]));
+//             }
            
-        //}
-         v.push_back(1);
-         ans.push_back(v);
+//         //}
+//          v.push_back(1);55678
+//          ans.push_back(v);
 
-    }
-    t++;
+//     }
+//     t++;
 
-    }
-    return ans;
+//     }
+//     return ans;
+        
+        
+        //efficient solution
+        vector<vector<int>> t(n);
+        
+        for(int i=0;i<n;i++)
+        {
+            t[i].resize(i+1);
+            t[i][0]=t[i][i]=1;
+            for(int j=1;j<i;j++)
+            {
+                t[i][j]=t[i-1][j-1]+t[i-1][j];
+            }
+        }
+        return t;
     }
 };
